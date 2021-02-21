4c297cc460acf5553436b55b6eadda3d74451b08
ksdevinney <katie@devinney.org>
2021-02-21 11:41:59 -0600

    the prompts work

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //special characters array
-var specialCharacters = ['!','"','#','$','%','&',''','(',',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_','`','{','|','}','~']
+var specialCharacters = ['!','"','#','$','%','&','+']
+
+var index = Math.floor(Math.random() * specialCharacters.length);
+
+//generate special character from array
+var useSpecial = specialCharacters[index];
+
+//index gives an integer
+console.log(index)
+
+//prompts for criteria
+var charactersInPassword = confirm('Use special characters?');
+  /*
+  if {
+    
+  }
+}*/
+
+var useUpper = confirm('Use uppercase letters?');
+
+var useLower = confirm('Use lowercase letters?');
+
+var useNumbers = confirm('Use numbers?');
+
+var totalCharacters = prompt('How many characters?','8-128');
 
 // Write password to the #password input
 function writePassword() {
