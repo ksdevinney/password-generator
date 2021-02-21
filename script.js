b597c89c4d87d3981b64c1e886826c3522cbaa82
ksdevinney <katie@devinney.org>
2021-02-21 10:41:08 -0600

    special characters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//special characters array
+var specialCharacters = ['!','"','#','$','%','&',''','(',',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',']','^','_','`','{','|','}','~']
 
 // Write password to the #password input
 function writePassword() {
