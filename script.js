194e72749114332480913abd213938a533d403a3
ksdevinney <katie@devinney.org>
2021-02-26 19:57:03 -0600

    prompts begin on button click, removed some unneeded code

--- script.js
+++ script.js
@@ -2,91 +2,81 @@
 var generateBtn = document.querySelector("#generate");
 
 //put something in the box
+/*
 const password = document.getElementById("password");
-const length = document.getElementById(passLength);
+const length = document.getElementById(passLength); */
 
 //options to include in the password
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseChracters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//functions for generating random characters
-function randomSpecial() {
-  Math.floor(Math.random() * specialCharacters.length);
-}
+//declare variables
+/*var useSpecial;
 
-function randomLower() {
-  Math.floor(Math.random() * lowerCaseCharacters.length);
-}
+var useUpper;
 
-function randomUpper() {
-  Math.floor(Math.random() * upperCaseCharacters.length);
-}
+var useLower;
 
-function randomNumber() {
-  Math.floor(Math.random() * numericCharacters.length);
-}
+var useNumbers; */
 
+// Take a password length and list of characters and return a random password
+function generatePassword() {
+  
+  //prompts for criteria
+  var useSpecial = confirm('Use special characters?');
 
-//prompts for criteria
-var useSpecial = confirm('Use special characters?');
+  var useUpper = confirm('Use uppercase letters?');
 
-var useUpper = confirm('Use uppercase letters?');
+  var useLower = confirm('Use lowercase letters?');
 
-var useLower = confirm('Use lowercase letters?');
+  var useNumbers = confirm('Use numbers?'); 
 
-var useNumbers = confirm('Use numbers?'); 
+  console.log(useSpecial, useUpper, useLower, useNumbers);
 
-console.log(useSpecial, useUpper, useLower, useNumbers);
+  //get a number for password length
+  var passLength = prompt('How many characters?','8-128');
 
-//get a number for password length
-var passLength = prompt('How many characters?','8-128');
+  console.log(passLength);
 
-console.log(passLength);
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (passLength < 8) {
+    alert("Password must be at least 8 characters.");
+  }
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (passLength > 128) {
+    alert("Password must be no more than 128 characters.");
+  }
 
-// Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
-if (passLength < 8) {
-  alert("Password must be at least 8 characters.");
-}
-// Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
-if (passLength > 128) {
-  alert("Password must be no more than 128 characters.");
-}
+  //alert if passLength isn't a number
+  if (isNaN(passLength) === true) {
+    alert('Password length must be provided as a number');
+  }
 
-//alert if passLength isn't a number
-if (isNaN(passLength) === true) {
-  alert('Password length must be provided as a number');
-}
+  //this triggers if one is not selected?
 
-//this triggers if one is not selected?
-/*
-if (useUpper === false && useLower === false && useNumbers === false && useSpecial === false) {
-  alert("Please select at least one character type.")
-}
-*/
+  if (!useUpper && !useLower && !useNumbers && !useSpecial) {
+    alert("Please select at least one character type.")
+    return "invalid password"
+  }
+  
 
-// Generate a list of characters depending on toggles
-function checkCharacters(useSpecial, useUpper, useLower, useNumbers) {
+  
   var pickedCharacters = [];
 
   if (useSpecial) {
     pickedCharacters = pickedCharacters.concat(specialCharacters);
   } 
   if (useUpper) {
-    pickedCharacters = pickedCharacters.concat(upperCaseChracters);
+    pickedCharacters = pickedCharacters.concat(upperCaseCharacters);
   }
   if (useLower) {
-    pickedCharacters = pickedCharacters.concat(upperCaseChracters);
+    pickedCharacters = pickedCharacters.concat(lowerCaseCharacters);
   }
   if (useNumbers) {
     pickedCharacters = pickedCharacters.concat(numericCharacters);
   }
-  return pickedCharacters
-}
-
-// Take a password length and list of characters and return a random password
-function generatePassword(passLength, pickedCharacters) {
 
   var passwordText = ""
 
@@ -100,8 +90,7 @@ function generatePassword(passLength, pickedCharacters) {
 
 // Write password to the #password input
 function writePassword() {
-  var pickedCharacters = checkCharacters(useSpecial, useUpper, useLower, useNumbers);
-  var password = generatePassword(passLength, pickedCharacters);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
