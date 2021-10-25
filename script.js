cbfc256056b5df146c5c87921fbdb44c22e8837b
ksdevinney <katie@devinney.org>
2021-10-25 15:37:17 -0500

    combine functions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Assignment Code
+// elements from html
 const generateBtn = document.getElementById("generate");
 const lengthEl = document.getElementById("length");
 const capitalsEl = document.getElementById("capital-letters");
 const specialsEl = document.getElementById("special-characters");
 const lowerEl = document.getElementById("lowercase-letters");
 const numberEl = document.getElementById("numbers");
-let passwordText = document.getElementById("password");
+let passEl = document.getElementById("password");
 
-//options to include in the password
+// options to include in the password
 const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+const specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 const lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -21,24 +21,19 @@ generateBtn.addEventListener("click", () => {
   const useNumbers = numberEl.checked;
   const passLength = +lengthEl.value;
 
-  // console.log(useSpecial, useUpper, useLower, useNumbers, passLength);
-  passwordText.innerText = generatePassword(useSpecial, useUpper, useLower, useNumbers, passLength)
-});
-
-// Take a password length and list of characters and return a random password
-function generatePassword(useSpecial, useUpper, useLower, useNumbers, passLength) {
   let passwordText = ""
 
   if (!useUpper && !useLower && !useNumbers && !useSpecial) {
     alert("Please select at least one character type");
     return ":(";
   }
-  
+
+  // character types to go in password
   let pickedCharacters = [];
 
   if (useSpecial) {
     pickedCharacters = pickedCharacters.concat(specialCharacters);
-  } 
+  }
   if (useUpper) {
     pickedCharacters = pickedCharacters.concat(upperCaseCharacters);
   }
@@ -49,6 +44,7 @@ function generatePassword(useSpecial, useUpper, useLower, useNumbers, passLength
     pickedCharacters = pickedCharacters.concat(numericCharacters);
   }
 
+  // loop to create password using characters array and selected length
   for (let i = 0; i < passLength; i++) {
     if (passLength < 8 || passLength > 128 || isNaN(passLength) === true) {
       return ":("
@@ -56,5 +52,8 @@ function generatePassword(useSpecial, useUpper, useLower, useNumbers, passLength
       passwordText = passwordText + pickedCharacters[Math.floor(Math.random() * pickedCharacters.length)];
     }
   }
-  return passwordText;
-}
+  // return passwordText;
+
+  // put the new password in the box
+  passEl.innerHTML = passwordText;
+});
